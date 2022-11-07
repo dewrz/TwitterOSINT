2183705ec6a16e57511f4dbb399c15723f2fa0c7
Daniel McNally <114428694+dewrz@users.noreply.github.com>
2022-11-07 17:07:40 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# TWITTER KEYS FOR THE API
+
+API_KEY = 'Add Your Key Here'
+API_KEY_SECRET = 'Add Your Key Here'
+
+BEARER_TOKEN = 'Add Your Key Here'
+
+ACCESS_TOKEN = 'Add Your Key Here'
+ACCESS_TOKEN_SECRET = 'Add Your Key Here'
